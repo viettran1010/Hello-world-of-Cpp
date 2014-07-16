f38105726a773c7ffdf593c1ffc804a52e30b126
Soh <Soh.ohira@gmail.com>
2014-07-16 15:11:22 +0900

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
+// name spaceを確保する
 using namespace std;
+//メイン ここから実行される!
 int main(){
+//こんちは
   cout << "Hello, World!" << endl;
+//変数 x
   int x;
-  cin >> x; 
+//cinが入力
+  cin >> x;
+/*一足す
+  x += 1 でも良い(自己代入)
+*/
   x ++;
   cout << x << endl;
-  return 0;
+  cout << "こんちは\n";
+  cout << "Hello\x61world!\n";
+  cout << 0xE;
+
+  return 0; 
+
 }
